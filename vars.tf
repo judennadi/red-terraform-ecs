f9bb8a2279d205c08e47af439b833c3c120f7473
judennadi <judeonnadi@gmail.com>
2023-02-08 15:53:24 +0100

    updated .tf files

--- vars.tf
+++ vars.tf
@@ -11,3 +11,40 @@ variable "environment" {
   description = "Project environment"
   type        = string
 }
+
+# vpc variables
+variable "vpc_cidr" {
+  description = "vpc cidr block"
+  type        = string
+}
+
+# public subnet variables
+variable "public_subnet_az1_cidr" {
+  description = "public subnet az1 cidr block"
+  type        = string
+}
+variable "public_subnet_az2_cidr" {
+  description = "public subnet az2 cidr block"
+  type        = string
+}
+
+# private app subnet variables
+variable "private_app_subnet_az1_cidr" {
+  description = "private app subnet az1 cidr block"
+  type        = string
+}
+variable "private_app_subnet_az2_cidr" {
+  description = "private app subnet az2 cidr block"
+  type        = string
+}
+
+# private data subnet variables
+variable "private_data_subnet_az1_cidr" {
+  description = "private data subnet az1 cidr block"
+  type        = string
+}
+variable "private_data_subnet_az2_cidr" {
+  description = "private data subnet az2 cidr block"
+  type        = string
+}
+
